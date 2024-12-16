755664fe512f8f0f13b83d41dadd7b80ec151d62
Asad Aslam <opworrier@gmail.com>
2024-12-16 13:54:10 +0500

    2 more section added

--- ecobazar/src/App.jsx
+++ ecobazar/src/App.jsx
@@ -1,18 +1,34 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero/Hero"
-
-
+import Hero from "./components/Hero/Hero";
+import Products from "./components/Products/Products";
+import TopProducts from "./components/TopProducts/TopProducts"
+import AOS from "aos";
+import "aos/dist/aos.css"
+import Banner from "./components/Banner/Banner";
 
 
 
 function App() {
+
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
   
 
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <Products/>
+      <TopProducts/>
+      <Banner/>
     </div>
   )
 }
